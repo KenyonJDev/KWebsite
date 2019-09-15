3749913d5182a9558d7827a314372232680709b4
Mark Tyers <aa7401@coventry.ac.uk>
2019-09-15 12:47:54 +0100

    completed unit tests for module

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,7 @@
 
 'use strict'
 
+const bcrypt = require('bcrypt-promise')
 const fs = require('fs-extra')
 const mime = require('mime-types')
 const sqlite = require('sqlite-async')
@@ -18,24 +19,42 @@ module.exports = class User {
 		})()
 	}
 
-	async register(username, password, filename, filetype) {
-		const fileExtension = mime.extension(filetype)
-		await fs.copy(path, `public/avatars/${username}.${fileExtension}`)
-		password = await bcrypt.hash(password, saltRounds)
-		const sql = `INSERT INTO users(user, pass) VALUES("${username}", "${password}")`
-		await this.db.run(sql)
-		return true
+	async register(user, pass) {
+		try {
+			if(user.length === 0) throw new Error('missing username')
+			if(pass.length === 0) throw new Error('missing password')
+			let sql = `SELECT COUNT(id) as records FROM users WHERE user="${user}";`
+			const data = await this.db.get(sql)
+			if(data.records !== 0) throw new Error(`username "${user}" already in use`)
+			pass = await bcrypt.hash(pass, saltRounds)
+			sql = `INSERT INTO users(user, pass) VALUES("${user}", "${pass}")`
+			await this.db.run(sql)
+			return true
+		} catch(err) {
+			throw err
+		}
+	}
+
+	async uploadPicture(path, mimeType) {
+		const extension = mime.extension(mimeType)
+		console.log(`path: ${path}`)
+		console.log(`extension: ${extension}`)
+		//await fs.copy(path, `public/avatars/${username}.${fileExtension}`)
 	}
 
 	async login(username, password) {
-		let sql = `SELECT count(id) AS count FROM users WHERE user="${body.user}";`
-		const records = await this.db.get(sql)
-		if(!records.count) throw new Error(`username "${username}" not found`)
-		sql = `SELECT pass FROM users WHERE user = "${body.user}";`
-		const record = await db.get(sql)
-		const valid = await bcrypt.compare(body.pass, record.pass)
-		if(valid == false) throw new Error(`invalid password for account "${username}"`)
-		return true
+		try {
+			let sql = `SELECT count(id) AS count FROM users WHERE user="${username}";`
+			const records = await this.db.get(sql)
+			if(!records.count) throw new Error(`username "${username}" not found`)
+			sql = `SELECT pass FROM users WHERE user = "${username}";`
+			const record = await this.db.get(sql)
+			const valid = await bcrypt.compare(password, record.pass)
+			if(valid === false) throw new Error(`invalid password for account "${username}"`)
+			return true
+		} catch(err) {
+			throw err
+		}
 	}
 
 }
