f06f180ace96f0b3d48fb6240053cce498293917
KenyonJ <KenyonJ@coventry.ac.uk>
2019-11-07 14:24:33 +0000

    fixed issue with variable name

--- modules/User_songs.js
+++ modules/User_songs.js
@@ -5,7 +5,7 @@ module.exports = class User_songs {
 
 	constructor(dbName = ':memory:') {
 		return (async() => {
-			this.db = await sqlite.open(user_songs)
+			this.db = await sqlite.open(dbName)
 			// we need this table to store the user songs
 			const sql = 'CREATE TABLE IF NOT EXISTS users (song_id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, FOREIGN KEY(song_id) REFERENCES Songs(song_id), FOREIGN KEY(user) REFERENCES Users(user);'
 			await this.db.run(sql)
