d8f7bca9f87add788301b327ed6a9d84d7e22b02
wlodarsb <wlodarsb@coventry.ac.uk>
2019-11-07 15:36:46 +0000

    Created song class
    Created the Song class. This class will be used for inserting songs into the database.

--- modules/song.js
+++ modules/song.js
@@ -1,15 +1,29 @@
 'use strict'
 
 const sqlite = require('sqlite-async')
+const metadata = require('music-metadata')
+
+const musicPath = 'public\\music\\'
 
 module.exports = class Song {
 	constructor(dbname = ':memory:') {
-		return async() => {
+		return (async() => {
 			this.db = await sqlite.open(dbname)
 			// eslint-disable-next-line max-len
 			const sql = 'CREATE TABLE IF NOT EXISTS songs(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, artist TEXT, publisher TEXT, year INTEGER)'
 			await this.db.run(sql)
 			return this
+		})()
+	}
+
+	async add(fileName) {
+		try {
+			const path = `${musicPath}${fileName}`
+			const data = await metadata.parseFile(path)
+			console.log(data)
+		} catch(err) {
+			console.error(err)
 		}
 	}
 }
+
