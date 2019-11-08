accf44db97cecfa69e496b68673c25ef36c8973f
KenyonJ <KenyonJ@coventry.ac.uk>
2019-11-08 10:06:56 +0000

    Added new file Playlist_songs.js and fixed coding errors in old fies

--- modules/User_playlists.js
+++ modules/User_playlists.js
@@ -1,12 +1,12 @@
 'use strict'
 
-module.exports = class User_playlists {
+module.exports = class UserPlaylist {
 
 	constructor(dbName = ':memory:') {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
 			// we need this table to store the user songs
-			const sql = 'CREATE TABLE IF NOT EXISTS users playlist_id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, FOREIGN KEY(playlist_id) REFERENCES Playlists(playlist_id), FOREIGN KEY(user) REFERENCES Users(user);'
+			const sql = 'CREATE TABLE IF NOT EXISTS user_playlists (playlist_id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, FOREIGN KEY(playlist_id) REFERENCES Playlists(playlist_id), FOREIGN KEY(user) REFERENCES Users(user);'
 			await this.db.run(sql)
 			return this
 		})()
--- modules/User_songs.js
+++ modules/User_songs.js
@@ -10,7 +10,7 @@ module.exports = class userSong {
 			this.db = await sqlite.open(dbName)
 			// we need this table to store the user songs
 			// eslint-disable-next-line max-len
-			const sql = 'CREATE TABLE IF NOT EXISTS users (song_id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, FOREIGN KEY(song_id) REFERENCES Songs(song_id), FOREIGN KEY(user) REFERENCES Users(user);'
+			const sql = 'CREATE TABLE IF NOT EXISTS user_songs (song_id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, FOREIGN KEY(song_id) REFERENCES Songs(song_id), FOREIGN KEY(user) REFERENCES Users(user);'
 			await this.db.run(sql)
 			return this
 		})()
