46fb1eddbc46ea9d03694ff1d32753cadc6a08da
wlodarsb <wlodarsb@coventry.ac.uk>
2019-11-09 10:24:40 +0000

    Attempting to fix tests
    Some module is causing issues with Jest. Pushing changes to work on the issue on another computer.

--- unit tests/song.spec.js
+++ unit tests/song.spec.js
@@ -4,34 +4,39 @@ const Song = require('../modules/song')
 const mock = require('mock-fs')
 const ID3 = require('node-id3')
 
-beforeAll( async() => {
-    mock({
-        'public/music': {
-            'song.mp3': ''
-        }
-    })
-    const tags = {
-        title: 'Hyori Ittai',
-        artist: 'Yuzu',
-        album: 'Shinsekai'
-    }
-    const success = ID3.update(tags, 'public/music/song.mp3')
-    if(!success) throw new Error('tag insertion not successful')
-    console.log(ID3.read('public/music/song.mp3'))
+beforeAll( () => {
+	mock({
+		'public': {
+			'music': {
+				'song.mp3': ''
+			}
+		}
+	})
+	const tags = {
+		title: 'Hyori Ittai',
+		artist: 'Yuzu',
+		album: 'Shinsekai'
+	}
+	const success = ID3.update(tags, 'public/music/song.mp3')
+	if(!success) throw new Error('tag insertion not successful')
+	console.log(ID3.read('public/music/song.mp3'))
 })
 
-afterAll( async() => {
-    mock.restore()
+afterAll( () => {
+	mock.restore()
 })
 
-describe('add()', async() => {
-
-    beforeEach( async() => {
-        const song = await new Song() 
-    })
-    
-    test('reading song', async done => {
-        expect.assertions(1)
-        await song.add('song.mp3')
-    })
+describe('add()', () => {
+	test('reading song', async done => {
+		expect.assertions(1)
+		try {
+			const song = await new Song()
+			const tags = await song.add('song.mp3')
+			expect(tags).not(undefined)
+		} catch(err) {
+			done.fail('no data returned')
+		} finally {
+			done()
+		}
+	})
 })
