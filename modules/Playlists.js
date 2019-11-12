5e0e35216cfd0b4439307cbc01504913a9abf6f9
KenyonJ <KenyonJ@Coventry.ac.uk>
2019-11-12 00:20:17 +0000

    Fixed issues flagged by ESLint
    Fixed formatting issues that were shown by ESlint
    
    Fixed a mistake whereby 'sql' was not defined.

--- modules/Playlists.js
+++ modules/Playlists.js
@@ -8,33 +8,35 @@ module.exports = class Playlists {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
 			// we need this table to store the user songs
-			const sql = 'CREATE TABLE IF NOT EXISTS playlists(playlist_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT;'
+			const sql = `CREATE TABLE IF NOT EXISTS playlists(
+				playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+				name TEXT,
+				description TEXT;`
 			await this.db.run(sql)
 			return this
 		})()
 	}
-
 	async create(name, description) {
-	try {
-		if(name.length === 0) throw new Error('missing name')
-		if(description.length === 0) throw new Error('missing description')
-		const data = await this.db.get(sql)
-		sql = `INSERT INTO playlists(name, description) VALUES("${name}", "${description}")`
-		await this.db.run(sql)
-		return true
-	} catch(err) {
-		throw err
+		try {
+			if(name.length === 0) throw new Error('missing name')
+			if(description.length === 0) throw new Error('missing description')
+			let sql = `SELECT COUNT(id) as records FROM playlists WHERE name="${name}";`
+			sql = `INSERT INTO playlists(name, description) VALUES("${name}", "${description}")`
+			await this.db.run(sql)
+			return true
+		} catch(err) {
+			throw err
 		}
 	}
 
 	async delete(name, description) {
 		try {
-			const data = await this.db.get(sql)
+			let sql = `SELECT COUNT(id) as records FROM playlists WHERE name="${name}";`
 			sql = `DELETE FROM playlists(name, description) VALUES("${name}", "${description}")`
 			await this.db.run(sql)
 			return true
 		} catch(err) {
 			throw err
-			}
 		}
+	}
 }
