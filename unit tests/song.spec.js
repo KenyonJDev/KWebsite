094cbd297bcd89d43a8d971cd54efa70f27db966
wlodarsb <wlodarsb@coventry.ac.uk>
2019-11-13 13:34:29 +0000

    Working on unit tests
    Added unit tests for new functions.

--- unit tests/song.spec.js
+++ unit tests/song.spec.js
@@ -16,13 +16,14 @@ afterAll( () => {
 	mock.restore()
 })
 
-describe('add()', () => {
+describe('extractTags()', () => {
 
 	test('reading valid song', async done => {
 		expect.assertions(1)
 		const song = await new Song()
-		const tags = await song.add('music/song.mp3')
+		const tags = await song.extractTags('music/song.mp3')
 		await expect(tags).toEqual({
+			file: 'song.mp3',
 			title: 'test title',
 			artist: 'test artist',
 			year: 2010
@@ -34,8 +35,16 @@ describe('add()', () => {
 		expect.assertions(1)
 		const song = await new Song()
 		const invalidFile = 'files/text.txt'
-		await expect(song.add(invalidFile))
+		await expect(song.extractTags(invalidFile))
 			.rejects.toEqual(Error(`file '${invalidFile}' is not an .mp3 file`))
+		done()
+	})
+
+	test('passing no arguments', async done => {
+		expect.assertions(1)
+		const song = await new Song()
+		await expect(song.extractTags())
+			.rejects.toEqual(Error('no arguments passed'))
 		done()
 	})
 
@@ -43,30 +52,58 @@ describe('add()', () => {
 		expect.assertions(1)
 		const song = await new Song()
 		const invalidPath = 'public/music/invalid_song.mp3'
-		await expect(song.add('public/music/invalid_song.mp3'))
+		await expect(song.extractTags('public/music/invalid_song.mp3'))
 			.rejects.toEqual(Error(`file '${invalidPath}' does not exist`))
 		done()
 	})
 })
 
-describe('get()', () => {
-	test('retrieving a valid song', async done => {
+describe('add', () => {
+	test('adding valid tags', async done => {
 		expect.assertions(1)
 		const song = await new Song()
 		const path = 'music/song.mp3'
-		await song.add(path)
-		const data = await song.get(1)
+		const tags = await song.extractTags(path)
+		const data = await song.add(tags)
 		await expect(data.file).toBe('song.mp3')
 		done()
 	})
-	test('retrieving invalid key', async done => {
+
+	test('passing nothing', async done => {
 		expect.assertions(1)
 		const song = await new Song()
-		const path = 'music/song.mp3'
-		await song.add(path)
-		const key = 2
-		await expect(song.get(key))
-			.rejects.toEqual(Error(`record for key ${key} does not exist`))
+		await expect(song.add())
+			.rejects.toEqual(Error('no tags argument passed'))
+		done()
+	})
+
+	test('passing empty object', async done =>{
+		expect.assertions(1)
+		const song = await new Song()
+		const emptyObj = {}
+		await expect(song.add(emptyObj))
+			.rejects.toEqual(Error('no filename in tags object'))
+		done()
+	})
+
+	test('passing object with missing tags', async done => {
+		expect.assertions(4)
+		const song = await new Song()
+		const objects = [
+			{tags: {title: 'a', artist: 'b', year: 1}, err: 'filename'}, // missing filename
+			{tags: {file: 'a', artist: 'b', year: 1}, err: 'title'}, // missing title
+			{tags: {file: 'a', title: 'b', year: 1}, err: 'artist'}, // missing artist
+			{tags: {file: 'a', title: 'b', artist: 'c'}, err: 'year'} // missing year
+		]
+		for(const obj of objects) {
+			await expect(song.add(obj.tags))
+				.rejects.toEqual(Error(`no ${obj.err} in tags object`))
+		}
 		done()
 	})
 })
+
+/* describe('get()', () => {
+
+})
+ */
