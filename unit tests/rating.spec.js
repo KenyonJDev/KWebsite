7c4e20d5e66013c3ec4da03527a9162ca3d6dbdf
logarint <logarint@coventry.ac.uk>
2019-11-18 22:34:51 +0000

    Created unit testing for ratings(incomplete)

---
+++ unit tests/rating.spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Song = require('../modules/Rating')
+const mock = require('mock-fs')
+const fs = require('fs')
+
+// Constants used in tests
+const validRating = [1, 2, 3, 4, 5]
+const emptyRating = []
+const appropriateComment = 'test comment'
+
