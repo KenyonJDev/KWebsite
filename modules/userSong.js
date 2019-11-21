4100e07aa028a0da741925d3d08f3d91f8a0e76a
wlodarsb <wlodarsb@coventry.ac.uk>
2019-11-21 14:39:13 +0000

    Modularised userSong
    I have moved repeated tests into songChecks.js to make the file take up fewer lines.

--- modules/userSong.js
+++ modules/userSong.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const sqlite = require('sqlite-async')
+const check = require('./songChecks')
 
 class UserSong {
 	constructor(dbName = ':memory:') {
@@ -17,32 +18,36 @@ class UserSong {
 	}
 
 	async link(user, song) {
-		if(user === undefined) throw new Error('user ID is undefined')
-		if(song === undefined) throw new Error('song ID is undefined')
+		await check.user(user)
+		await check.song(song)
 		const sql = `INSERT INTO userSongs(userID, songID) VALUES("${user}", "${song}")`
 		await this.db.run(sql)
 		return true
 	}
 
 	async get(user) {
-		if(user === undefined) throw new Error('user ID is undefined')
-		if(isNaN(user)) throw new Error('user ID is not a number')
-		const sql = `SELECT FROM userSongs WHERE userID=${user}`
+		await check.user(user)
+		const sql = `SELECT songID FROM userSongs WHERE userID=${user}`
 		const data = await this.db.all(sql)
+		if(data.length === 0) throw new Error(`user ID ${user} does not exist`)
 		return data
 	}
 
 	async check(song) {
-		if(song === undefined) throw new Error('song ID is undefined')
+		await check.song(song)
 		const sql = `SELECT userID FROM userSongs WHERE songID=${song}`
 		let user = await this.db.get(sql)
+		if(user === undefined) throw new Error(`song ID ${song} does not exist`)
 		user = user.userID
 		return user
 	}
 
 	async remove(song) {
-		if(song === undefined) throw new Error('song ID is undefined')
-		const sql = `DELETE FROM userSongs WHERE songID=${song}`
+		await check.song(song)
+		let sql = `SELECT songID FROM userSongs WHERE songID=${song}`
+		const data = await this.db.get(sql)
+		if(data === undefined) throw new Error(`song ID ${song} does not exist`)
+		sql = `DELETE FROM userSongs WHERE songID=${song}`
 		await this.db.run(sql)
 		return true
 	}
