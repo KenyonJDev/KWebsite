6a770e89342f877800ace8641aea85cd7f114e70
KenyonJ <KenyonJ@coventry.ac.uk>
2019-11-22 11:03:13 +0000

    Updating to include async
    I have included the async method to my variable

--- modules/ImageCrop.js
+++ modules/ImageCrop.js
@@ -1,25 +1,28 @@
 'use strict'
 const sharp = require('sharp')
 const fs = require('fs')
-const fn = require('fn')
 const height = 255
 const width = 255
-const File = fs.readFileSync(fn)
 /**
  * @file Image cropping script
  * @author Joshua Kenyon <kenyonJ@uni.coventry.ac.uk>
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 /**
 	 * @description Resizes image to 255,255 while maintaining aspect ratio.
-	 * @param {File} sharp - The original image.
-	 * @returns {File} - Resized image.
+	 * @param {filePath} image - The original image.
+	 * @returns {filePath} - Resized image.
 	 */
-sharp(File)
-	.resize(width, height, {
-		fit: 'cover'
-	})
-	.toFile(File)
-	.then(console.log)
-	.catch(console.error)
+const crop = async filePath => {
+	const file = fs.readFileSync(filePath)
+	sharp(file)
+		.resize(width, height, {
+			fit: 'cover'
+		})
+		.toFile(filePath)
+		.then(console.log)
+		.catch(console.error)
+}
+
+module.exports = crop
