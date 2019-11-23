a616ad5af5e6a68808302a6bb8e2a91a02aa8610
logarint <logarint@coventry.ac.uk>
2019-11-23 23:35:09 +0000

    Some unit tests done, but more to come

--- unit tests/playlists.spec.js
+++ unit tests/playlists.spec.js
@@ -3,20 +3,19 @@
 
 const Playlist = require('../modules/playlists.js')
 
-describe('Create()', () => {
+describe('create()', () => {
 
 	test('Create a new playlist', async done => {
 		expect.assertions(1)
 		const playlist = await new Playlist()
-		const Create = await playlist.Create('Playlist', 'description')
-		expect(Create).toBe(true)
+		const create = await playlist.create('Playlist', 'description')
+		expect(create).toBe(1)
 		done()
 	})
-
 	test('test for blank name', async done => {
 		expect.assertions(1)
 		const playlist = await new Playlist()
-		await expect( playlist.Create('', 'description') )
+		await expect( playlist.create('', 'description') )
 			.rejects.toEqual( Error('missing name') )
 		done()
 	})
@@ -24,8 +23,33 @@ describe('Create()', () => {
 	test('test for blank description', async done => {
 		expect.assertions(1)
 		const playlist = await new Playlist()
-		await expect( playlist.Create('Playlist', '') )
+		await expect( playlist.create('Playlist', '') )
 			.rejects.toEqual( Error('missing description') )
 		done()
 	})
 })
+/*
+describe('get()', () => {
+	test('retrieving valid playlist', async done => {
+		expect.assertions(1)
+		const playlist = new Playlist()
+		//const create = playlist.create('Playlist', 'Description')
+		const id = 1
+		const confirm = await playlist.get(id)
+		await expect(confirm).toEqual('1', 'test name', 'test description')
+		done()
+	})
+})*/
+/*
+describe('delete()', () => {
+
+	test('deleting a valid playlist', async done => {
+		expect.assertions(1)
+		const playlist = await new Playlist()
+		//await playlist.create('Playlist' ,'Description')
+		const id = 1
+		const confirm = await playlist.delete(id)
+		await expect(confirm).toEqual(true)
+		done()
+	})
+})*/
