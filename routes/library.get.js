66bafb23ffcafcf02d260ef206e5e8d454b68785
Bartlomiej Wlodarski (wlodarsb) <wlodarsb@coventry.ac.uk>
2019-12-02 04:31:09 +0000

    Tweaked routes
    I have tweaked some minor issues with the routes. For example, I fixed
    an issue with the library route attempting to render even though the
    user is not logged in. For playlists, I fixed a redirect issue.

--- routes/library.get.js
+++ routes/library.get.js
@@ -4,17 +4,23 @@ const Playlists = require('../modules/Playlists')
 const UserPlaylist = require('../modules/User_playlists')
 
 const getLibrary = async(ctx, dbName) => {
-	const data = []
-	const playlists = await new Playlists(dbName)
-	const user = await new UserPlaylist(dbName)
-	const userplaylists = await user.getUserPlaylists(ctx.session.id)
-	const empty = await user.getUserPlaylists(ctx.session.id)
-	const list = []
-	for (const id of userplaylists) list.push(await playlists.getPlaylist(id))
-	if (empty.length === 0) data.empty = true
-	console.log(list)
-	data.playlists = list
-	await ctx.render('library', data)
+	try {
+		if (ctx.session.authorised !== true) await ctx.redirect('/login?msg=you need to login')
+		const data = []
+		const playlists = await new Playlists(dbName)
+		const user = await new UserPlaylist(dbName)
+		const userplaylists = await user.getUserPlaylists(ctx.session.id)
+		const empty = await user.getUserPlaylists(ctx.session.id)
+		const list = []
+		for (const id of userplaylists) list.push(await playlists.getPlaylist(id))
+		if (empty.length === 0) data.empty = true
+		console.log(list)
+		data.playlists = list
+		await ctx.render('library', data)
+	} catch(err) {
+		console.log(err)
+		await ctx.render('error', {msg: err.message})
+	}
 }
 
 module.exports = getLibrary
